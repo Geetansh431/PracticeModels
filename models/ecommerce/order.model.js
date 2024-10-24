6f087ff1ca7379a814e223ebb603308e00f2f3d1
Geetansh431 <geetanshg2@gmail.com>
2024-10-24 23:43:19 +0530

    Added Ecommerce model

--- models/ecommerce/order.model.js
+++ models/ecommerce/order.model.js
@@ -1,3 +1,29 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
-const orderSchema = new mongoose.Schema({},{timestamps:true})
+const orderItemSchema = new mongoose.Schema({
+    productId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"Product",
+    },
+    quantity:{
+        type:Number,
+        required:true,
+    },
+})
+
+const orderSchema = new mongoose.Schema({
+    orderPrice:{
+        type:Number,
+        required:true
+    },
+    customer:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+    },
+    orderItems: {
+        type: []
+    }
+},{timestamps:true})
+
+
+export const Order = mongoose.model("Order",orderSchema)
--- models/ecommerce/product.model.js
+++ models/ecommerce/product.model.js
@@ -1,19 +1,38 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-    description:{
-        type:String,
-        required:true,
+const productSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
     },
-    name:{
-        type:String,
-        required:true,
+    name: {
+      type: String,
+      required: true,
     },
-    productImage:{
-        type:String,
-        
-    }
+    productImage: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    stock: {
+      default: 0,
+      type: Number,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-},{timestamps:true});
-
-export const Product = mongoose.model("Product",productSchema);
+export const Product = mongoose.model("Product", productSchema);
